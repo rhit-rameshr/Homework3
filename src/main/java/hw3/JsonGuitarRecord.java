5ca40ec492ac0c1685f9b742bbc411a430a8a416
rhit-wormmm <wormmm@rose-hulman.edu>
2026-01-22 14:19:51 -0500

    Pushed Guitar Record data

--- src/main/java/hw3/JsonGuitarRecord.java
+++ src/main/java/hw3/JsonGuitarRecord.java
@@ -1,4 +1,12 @@
 package main.java.hw3;
 
 public class JsonGuitarRecord {
+    String serialNumber;
+    double price;
+    String builder;
+    String model;
+    String type;
+    int numStrings;
+    String backWood;
+    String topWood;
 }
